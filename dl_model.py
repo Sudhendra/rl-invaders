f13b5be6c8cbabbf66fea90cb8c7467ec831106e
Sudhendra <sudhendrakambhamettu@gmail.com>
2022-08-15 15:15:07 +0530

    build dl model

--- dl_model.py
+++ dl_model.py
@@ -1,5 +1,22 @@
 # The deep learning model
 # func: learns from the env.observation_space pixels
-from imports import *
+# goal: create a filter which can detect objects in the game. ex: where the enemy pixel is, where the mther ship is etc.
 
+from imports import *
+from random_play import height, width, channels, actions
+
+def build_model(height, width, channels, actions):
+    model = Sequential() # creating a Sequential structure
+    model.add(Convolution2D(32, (8,8), strides=(4,4), activation='relu', input_shape=(3, height, width, channels)))
+    model.add(Convolution2D(64, (4,4), strides=(2,2), activation='relu'))
+    model.add(Convolution2D(64, (3,3), activation='relu'))
+    model.add(Flatten())
+    model.add(Dense(512, activation='relu'))
+    model.add(Dense(256, activation='relu'))
+    model.add(Dense(actions, activation='linear')) # output layer
+    return model
+
+# using height, width, channels and actions from env created in random_play.py
+model = build_model(height, width, channels, actions)
+print(model.summary())
 
--- random_play.py
+++ random_play.py
@@ -10,15 +10,18 @@ actions = env.action_space.n
 
 # running an env with random steps
 episodes = 5
-for episode in range(1, episodes+1):
-    state = env.reset()
-    done = False
-    score = 0
+def random_play(episodes):
+    for episode in range(1, episodes+1):
+        state = env.reset()
+        done = False
+        score = 0
 
-    while not done:
-        env.render("rgb_array")
-        action = random.choice([0,1,2,3,4,5]) # total 6 actions ['NOOP', 'FIRE', 'RIGHT', 'LEFT', 'RIGHTFIRE', 'LEFTFIRE']
-        n_state, reward, done, info = env.step(action) # taking the random action
-        score += reward
-    print("Episode: {} Score: {}".format(episode, score))
-env.close()
+        while not done:
+            env.render("rgb_array")
+            action = random.choice([0,1,2,3,4,5]) # total 6 actions ['NOOP', 'FIRE', 'RIGHT', 'LEFT', 'RIGHTFIRE', 'LEFTFIRE']
+            n_state, reward, done, info = env.step(action) # taking the random action
+            score += reward
+        print("Episode: {} Score: {}".format(episode, score))
+    env.close()
+
+# random_play(episodes)
