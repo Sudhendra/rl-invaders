418abd308ae4c3b85c6c664147251992ac6ab102
Sudhendra <sudhendrakambhamettu@gmail.com>
2022-08-15 13:36:06 +0530

    1

--- atari.py
+++ atari.py
@@ -1,3 +1,9 @@
 from imports import *
 
 # Testing a random env with open AI gym
+env = gym.make("ALE/SpaceInvaders-v5")
+height, width, channels = env.observation_space.shape
+actions = env.action_space.n
+
+# getting the action names. Understanding better
+print(env.unwrapped.get_action_meanings())
