8d0d09b2df194290d96572abbbfe34d9e9ad18f4
Sudhendra <sudhendrakambhamettu@gmail.com>
2022-08-15 14:03:18 +0530

    3

---
+++ dl_model.py
@@ -0,0 +1,5 @@
+# The deep learning model
+# func: learns from the env.observation_space pixels
+from imports import *
+
+
--- imports.py
+++ imports.py
@@ -1,2 +1,7 @@
+import imp
 import gym
 import random
+import numpy
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Convolution2D, Flatten
+from tensorflow.keras.optimizers import Adam
